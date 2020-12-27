1fd6ae31fb642ca2d07f76826733b577715d503b
Ipek Ozkuyumcu <ipekozkuyumcu@hotmail.com>
2020-12-27 18:32:33 +0300

    feat: unit test sample

---
+++ app/src/test/java/org/dpppt/android/app/MainActivityTest.java
@@ -0,0 +1,24 @@
+package org.dpppt.android.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+
+    @Test
+    public void onSaveInstanceState() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+}
---
+++ app/src/test/java/org/dpppt/android/app/MainApplicationTest.java
@@ -0,0 +1,12 @@
+package org.dpppt.android.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainApplicationTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
